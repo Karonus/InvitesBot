0ac90bbaac8a64fabcacb75c34104ae016e91c65
Karonus <46893740+Karonus@users.noreply.github.com>
2021-10-13 21:24:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TOKEN = ""  # Telegram bot token
+message_text = "Join our chat for further communication."  # Message text
 button_text = "Join to chat"  # Message button text
 button_link = "https://t.me/"  # Link to your chat
