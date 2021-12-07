b1c3d5a01c2eb7908e984187958cfd869d650fc3
Karonus <46893740+Karonus@users.noreply.github.com>
2021-12-07 20:24:03 +0300

    Expansion of functionality

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-TOKEN = ""  # Telegram bot token
+TOKEN = "1672163664:AAFMqskH6ZesAup09Wao8aHGLYvBd9uoiMU"  # Telegram bot token
 message_text = "Join our chat for further communication."  # Message text
 button_text = "Join to chat"  # Message button text
 button_link = "https://t.me/"  # Link to your chat
+
+autoban_user_channels = True
